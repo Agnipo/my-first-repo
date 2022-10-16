58eca259a77249f962fbb3ddac92c9a5f8004331
Agnipo <agnieszka.popiesz1@gmail.com>
2022-10-16 13:16:55 +0200

    PROJEKTFINAL

---
+++ PROJEKT2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ocena;
+    cout<<"Ktory miesiac wybierasz ";
+    cin>>ocena;
+
+    switch(ocena)
+    {
+     case 1:
+            cout<<"Styczen";
+            break;
+     case 2:
+            cout<<"Luty";
+            break;
+     case 3:
+            cout<<"Marzec";
+            break;
+     case 4:
+            cout<<"Kwiecien";
+            break;
+     case 5:
+            cout<<"Maj";
+            break;
+     default:
+            cout<<"Podaj liczbe 1-5!";
+            break;
+    }
+    system("PAUSE");
+    return 0;
+
+}
+  default:
+            cout<<"Podaj liczbe 1-5!";
+            break;
